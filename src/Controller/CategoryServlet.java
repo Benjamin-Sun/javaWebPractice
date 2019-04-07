b47dbe9f3add013f1030cb447cd56c3a78c45abf
Benjamin_Sun_ <912249151@qq.com>
2019-04-07 14:34:14 +0800

    final fix

--- src/Controller/CategoryServlet.java
+++ src/Controller/CategoryServlet.java
@@ -3,6 +3,7 @@ package Controller;
 import Entity.Category;
 import Repository.CategoryRepository;
 import Repository.imp.CategoryRepositoryImp;
+import Service.CategoryService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,7 +18,8 @@ import java.util.List;
 
 @WebServlet(name = "CategoryServlet", urlPatterns = "*.cate")
 public class CategoryServlet extends HttpServlet {
-    private CategoryRepository categoryRepository = new CategoryRepositoryImp();
+
+    private CategoryService categoryService = new CategoryService();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uri = request.getRequestURI();
@@ -28,8 +30,8 @@ public class CategoryServlet extends HttpServlet {
 
         //通过反射来实现方法的调用
         try {
-            Method method = this.getClass().getMethod(path, HttpServletRequest.class, HttpServletResponse.class);
-            method.invoke(this, request, response);
+            Method method = CategoryService.class.getMethod(path, HttpServletRequest.class, HttpServletResponse.class);
+            method.invoke(categoryService, request, response);
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -44,52 +46,5 @@ public class CategoryServlet extends HttpServlet {
         doPost(request, response);
     }
 
-    public void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
-        List<Category> lists = categoryRepository.selectAll();
-        req.setAttribute("cates", lists);
-        req.getRequestDispatcher("catelist.jsp").forward(req, resp);
-    }
 
-    public void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
-        String cname = req.getParameter("cname");
-        Category category = new Category();
-        category.setCname(cname);
-
-        int i = categoryRepository.insert(category);
-
-        resp.sendRedirect("list.cate");
-
-    }
-
-    public void find(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
-        String cid = req.getParameter("id");
-        if (cid != null) {
-            Category category = categoryRepository.selectById(Integer.valueOf(cid));
-            req.setAttribute("cate", category);
-            req.getRequestDispatcher("cateupdate.jsp").forward(req, resp);
-
-        }
-        resp.sendRedirect("list.cate");
-    }
-
-    public void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
-        String cname = req.getParameter("cname");
-        String cid = req.getParameter("cid");
-        if (cid != null) {
-            Category category = new Category();
-            category.setCname(cname);
-            category.setCid(Integer.valueOf(cid));
-
-            int i = categoryRepository.update(category);
-        }
-        resp.sendRedirect("list.cate");
-    }
-
-    public void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
-        String cid = req.getParameter("id");
-        if (cid != null) {
-            int i= categoryRepository.deleteById(Integer.valueOf(cid));
-        }
-        resp.sendRedirect("list.cate");
-    }
 }
--- src/Service/CategoryService.java
+++ src/Service/CategoryService.java
@@ -1,4 +1,66 @@
 package Service;
 
+import Entity.Category;
+import Repository.CategoryRepository;
+import Repository.imp.CategoryRepositoryImp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 public class CategoryService {
+
+    private CategoryRepository categoryRepository = new CategoryRepositoryImp();
+
+    public void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
+        List<Category> lists = categoryRepository.selectAll();
+        req.setAttribute("cates", lists);
+        req.getRequestDispatcher("catelist.jsp").forward(req, resp);
+    }
+
+    public void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
+        String cname = req.getParameter("cname");
+        Category category = new Category();
+        category.setCname(cname);
+
+        int i = categoryRepository.insert(category);
+
+        resp.sendRedirect("list.cate");
+
+    }
+
+    public void find(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
+        String cid = req.getParameter("id");
+        if (cid != null) {
+            Category category = categoryRepository.selectById(Integer.valueOf(cid));
+            req.setAttribute("cate", category);
+            req.getRequestDispatcher("cateupdate.jsp").forward(req, resp);
+
+        }
+        resp.sendRedirect("list.cate");
+    }
+
+    public void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
+        String cname = req.getParameter("cname");
+        String cid = req.getParameter("cid");
+        if (cid != null) {
+            Category category = new Category();
+            category.setCname(cname);
+            category.setCid(Integer.valueOf(cid));
+
+            int i = categoryRepository.update(category);
+        }
+        resp.sendRedirect("list.cate");
+    }
+
+    public void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
+        String cid = req.getParameter("id");
+        if (cid != null) {
+            int i= categoryRepository.deleteById(Integer.valueOf(cid));
+        }
+        resp.sendRedirect("list.cate");
+    }
 }
